c59cc31db59058833ac571194b337718254b123a
uea-jose <jv.avilar@uea.edu.ec>
2024-03-10 15:09:40 -0500

    Version 5

---
+++ Tare13_funcion_cuidad_temep.py
@@ -0,0 +1,5 @@
+#Desarrollo de la Función:
+#Desarrolla una función en Python que calcule la temperatura promedio de una ciudad durante un período de tiempo.
+#La función debe ser capaz de manejar datos de temperaturas de múltiples ciudades y semanas.
+#Utiliza como base el ejemplo anterior, donde tenías datos de 3 ciudades durante 4 semanas.
+#Tu función debe recibir estos datos como parámetros y calcular la temperatura promedio de cada ciudad.
